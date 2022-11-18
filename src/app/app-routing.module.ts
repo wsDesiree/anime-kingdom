44548ff910ce12dbcb7965d0b8fa9a3d87a1ec7c
wsDesiree <110789227+wsDesiree@users.noreply.github.com>
2022-11-17 19:39:42 -0600

    edits

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 // import { AuthComponent } from './auth/auth.component';
 
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
 // import { ProfileComponent } from './profile/profile.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+// import { FeedComponent } from './feed/feed.component';
 
 const appRoutes: Routes = [
   { path:'', redirectTo: '/homepage', pathMatch: 'full' },
-  { path:'sign-up', component: SignUpComponent },
-  // { path:'homepage', component: HomepageComponent },
+  { path:'sign-up', component: SignUpComponent }
+  // { path:'feed', component: FeedComponent },
   // { path:'profile', component: ProfileComponent },
-  {
+  ,{ path:'login', component: LoginComponent }
+  ,{
     path: 'homepage',
     component: HomepageComponent,
 //     children: [
